270c2a6282b644b79aaba8f8de5606d137b0f06c
Per Olofsson <per.e.olofsson@gmail.com>
2021-01-21 16:50:46 +0100

    SImple log

--- JIRAPaste/main.js
+++ JIRAPaste/main.js
@@ -4,39 +4,16 @@ miro.onReady(() => {
             bottomBar: {
                 title: 'JIRAPaste',
                 svgIcon:
-                    '<text x="0" y="15"  height="20" fill="red">JIRA!</text>',
+                    '<text x="20" y="35" class="small">My</text>',
                 positionPriority: 1,
                 onClick: async () => {
-                    // Get selected widgets
-                    let selectedWidgets = await miro.board.selection.get()
-
-                    // Filter stickers from selected widgets
-                    let stickers = selectedWidgets.filter((widget) => widget.type === 'STICKER')
-
-                    // Delete selected stickers
-                    await miro.board.widgets.deleteById(stickers.map((sticker) => sticker.id))
-
-                    navigator.clipboard.readText()
-                        .then(text => {
-                            // `text` contains the text read from the clipboard
-                            // Create shapes from selected stickers
-                            miro.board.widgets.create(
-                                stickers.map((sticker) => ({
-                                    type: 'shape',
-                                    text: text,
-                                })),
-                            )
-
-                        })
-                        .catch(err => {
-                            // maybe user didn't grant access to read from clipboard
-                            console.log('Something went wrong', err);
-                        });
-
-                    // Show success message
-                    miro.showNotification('Stickers has been converted')
+                    buttonPressed();
                 },
             },
         },
     })
 })
+
+function buttonPressed() {
+    console.log('Button Pressed!');
+}
