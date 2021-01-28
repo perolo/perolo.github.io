9dd76e3c63c3f47160f466ec1067df4ff1c23048
Per Olofsson <per.e.olofsson@gmail.com>
2021-01-28 23:47:27 +0100

    remove

--- jira-sync/src/main.js
+++ jira-sync/src/main.js
@@ -92,7 +92,8 @@ async function syncWithSheet() {
         ).filter((card) => !!card.metadata[appId]);
         let tdtags = await miro.board.tags.get({title: 'To Do'});
 
-        remove  = await miro.board.tags.delete({title: 'To Do'}, tdtags.map(a => a.id));
+//        remove  = await miro.board.tags.delete({title: 'To Do'}, tdtags.map(a => a.id));
+        remove  = await miro.board.tags.delete(tdtags[0].id, tdtags[0].widgetIds);
         tdtags = await miro.board.tags.get({title: 'To Do'});
         const todocards = cards.find((card) => card.metadata[appId].statuscategory === 'To Do');
         thetag = await miro.board.tags.create({title: 'To Do', color: "#0000ff", todocards});
